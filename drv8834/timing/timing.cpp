77eeaca9b7d3ebaf402f9af2ae7fce451e483a14
Thomas Dodds <thomasmd1928@gmail.com>
2023-02-01 17:48:35 -0600

    change timing math
    math didn't really change, but before, it was figuring what OCR2A should be, but that's not changing on the fly. now, OCR2A is the constant, and the target number of ticks is what the end result we're solving for is.

--- drv8834/timing/timing.cpp
+++ drv8834/timing/timing.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 #include<cmath>
 
+// timer interval in microseconds
 #define TIMER_INTERVAL 4
-#define TIMER_RATIO 10
+// target output compare interrupt value
+// lower = more accurate stepping, higher = more CPU time for other things
+#define TIMER_OCR2A 31
 
 int rpm, microstepping;
 float revsec;
@@ -21,12 +24,12 @@ int main(){
 	fullsteps = revsec * 200;
 	microsteps = fullsteps * microstepping;
 	microsec_interval = 1000000 / microsteps;
-	count = std::floor(microsec_interval / TIMER_INTERVAL / TIMER_RATIO);
+	count = std::floor(microsec_interval / TIMER_INTERVAL / TIMER_OCR2A);
 
 	std::cout << "For " << rpm << " RPM (" << revsec << " revolutions per second)," << std::endl;
 	std::cout << "you need " << fullsteps << " full steps per second," << std::endl;
 	std::cout << "but at microstepping 1:" << microstepping << ", you need " << microsteps << " microsteps," << std::endl;
 	std::cout << "which translates to " << microsec_interval << " microsteps per second," << std::endl;
-	std::cout << "and for " << TIMER_RATIO << " timer ticks at " << TIMER_INTERVAL << " usec per tick, that's " << count << std::endl;
+	std::cout << "and for " << TIMER_OCR2A << " ticks at " << TIMER_INTERVAL << " usec per tick, that's " << count << " ticks." << std::endl;
 	return 0;
 }
