0080eca566be5205d71adf6a38846a2426f99520
Thomas Dodds <thomasmd1928@gmail.com>
2023-02-01 18:10:53 -0600

    implement math
    implemented the math from the timing program, removed unused variables, make comments more concise and specific, rewrite ISR

--- drv8834/drv8834.ino
+++ drv8834/drv8834.ino
@@ -1,33 +1,32 @@
 //Timer interrupt asynchronous control of multiple stepper motors with DRV8834
 //by Thomas Dodds for ATU IEEE
 
-//Driver wiring details:
+//Driver wiring details, common to all drivers:
 //Pin 1 GND to microcontroller-side GND
 //Pin 3-6 A1-B2 wired per usual to motor
 //Pin 7 GND and Pin 8 VMOT to motor-side power supply
 //Pin 10 M0 to microcontroller-side +5V
 //Pin 11 M1 to microcontroller-side GND
 //Pin 14 ~SLEEP to microcontroller Digital Pin 12
-//
+
+//Specific wiring for indicated drivers:
+//Front left Pin 15 STEP to Digital Pin 2
+//Front right Pin 15 STEP to Digital Pin 3
+//Rear left Pin 15 STEP to Digital Pin 4
+//Rear right Pin 15 STEP to Digital Pin 5
+
+//Front left Pin 16 DIR to Digital Pin 8
+//Front right Pin 16 DIR to Digital Pin 9
+//Rear left Pin 16 DIR to Digital Pin 10
+//Rear right Pin 16 DIR to Digital Pin 11
 
 //reference: https://community.robotshop.com/forum/t/arduino-101-timers-and-interrupts/13072
 //reference: https://www.ti.com/lit/ds/symlink/drv8834.pdf
 
-// 200 full steps at half step -> 400 half-steps per revolution
-// target 120 revolutions per minute -> 800 half steps per second
-// want a higher resolution than that though, say 10:1
-// so when step target is 10, it will rotate 120 rpm
-// when step target is 20, it will rotate 60 rpm
+#define STEP_RATIO 4 // microstepping ratio
+#define TIMER_OCR2A 31 // timer resolution. lower = accurate stepping, higher = more CPU time
+#define STEP_FULL 200 // full steps per revolution
 
-// to calc: 1200 / target = rpm
-//            or
-//          1200 / rpm = target
-
-#define STEP_RATE 1500 // minimum time between steps
-#define STEP_TIMER_RATIO 10 // speed resolution
-#define STEP_FULL_COUNT 200 // full steps per revolution
-#define STEP_RATIO 4 // microstepping mode (set in hardware, see wiring details)
-#define STEP_MICRO_COUNT (STEP_RATIO * STEP_FULL_COUNT)
 
 #define NUM_STEPPERS 4 // number of stepper drivers, generally don't change this
 
@@ -49,20 +48,23 @@
 #define STEPPER_SLEEP_PORT_DIR DDRB
 #define STEPPER_SLEEP_PIN 4
 
-uint8_t stepper_step_count[NUM_STEPPERS];
+boolean stepper_state[NUM_STEPPERS];
 uint8_t stepper_step_target[NUM_STEPPERS];
-boolean stepper_step_state[NUM_STEPPERS];
+uint8_t stepper_step_count[NUM_STEPPERS];
+boolean stepper_step[NUM_STEPPERS];
 
 // interrupt service routine for timer2 compare
 ISR(TIMER2_COMPA_vect) {
   for (uint8_t stepper_id = 0; stepper_id < NUM_STEPPERS; stepper_id++) {
-    if ((stepper_step_target[stepper_id] != 0) && ++stepper_step_count[stepper_id] >= stepper_step_target[stepper_id]) {
-      stepper_step_count[stepper_id] = 0;
-      stepper_step_state[stepper_id] = !stepper_step_state[stepper_id];
-      if(stepper_step_state[stepper_id] == true){
-        STEPPER_STEP_PORT |= 1 << stepper_id << STEPPER_STEP_START;
-      }else{
-        STEPPER_STEP_PORT &= ~(1 << stepper_id << STEPPER_STEP_START);
+    if(stepper_state[stepper_id]){
+      if(++stepper_step_count[stepper_id] >= stepper_step_target[stepper_id]){
+        stepper_step_count[stepper_id] = 0;
+        stepper_step[stepper_id] = !stepper_step[stepper_id];
+        if(stepper_step_state[stepper_id] == true){
+          STEPPER_STEP_PORT |= bit(stepper_id + STEPPER_STEP_START);
+        }else{
+          STEPPER_STEP_PORT &= ~bit(stepper_id + STEPPER_STEP_START);
+        }
       }
     }
   }
@@ -75,12 +77,8 @@ void setup() {
   TCCR2B = 0; // clear control register B
   TCNT2 = 0; // clear timer value
 
-  //1500 microseconds / ratio = 150 per timer tick, 10 to step
-  //16MHz / 64 prescaler = 250000, 1/250000 = 0.00004, or 4 microseconds
-  //divide wanted time per tick (150) by resolution of 4 microseconds and floor = timer OCR2A
-
-  OCR2A = floor(STEP_RATE / STEP_TIMER_RATIO / 4);
-  TCCR2B |= bit(WGM12) | bit(CS11) | bit(CS10); // CTC mode (clear at OC), 64 prescaler
+  OCR2A = TIMER2_OCR2A;
+  TCCR2B |= bit(WGM12) | bit(CS11) | bit(CS10); // CTC mode (clear at OC match), 64 prescaler
   TIMSK2 |= bit(OCIE2A); // ocie = output compare interrupt
   interrupts(); // enable interrupts
 
@@ -95,22 +93,23 @@ void setup() {
   STEPPER_SLEEP_PORT &= ~bit(STEPPER_SLEEP_PIN);
 }
 
-void setStepperSpeed(uint8_t stepper_id, byte speed, boolean direction){
+void setdir(uint8_t stepper_id, boolean direction){
   if(direction == true){
-    STEPPER_DIR_PORT |= 1 << STEPPER_DIR_START << stepper_id;
+    STEPPER_DIR_PORT |= bit(STEPPER_DIR_START + stepper_id);
   }else{
-    STEPPER_DIR_PORT &= ~(1 << STEPPER_DIR_START << stepper_id);
+    STEPPER_DIR_PORT &= ~bit(STEPPER_DIR_START + stepper_id);
   }
-  //stepper_
 }
-void setStepperSpeed(uint8_t stepper_id, int16_t speed){
-  uint8_t magnitude;
-  if(abs(speed) > 255){
-    magnitude = 255;
+void setRPM(uint8_t stepper_id, uint8_t rpm){
+  // given an rpm, how many ticks of the timer to step:
+  // 1000000 usec / (rpm / 60 s/m * full rotation steps * microstep ratio) / interrupt value / 4 usec
+  if(rpm != 0){
+    stepper_state[stepper_id] = true;
+    stepper_step_target = uint8_t(1000000 / (float(rpm) / 60.0 * STEP_FULL * STEP_RATIO) / 4 / TIMER_OCR2A);
   }else{
-    magnitude = abs(speed);
+    stepper_state[stepper_id] = false;
+    stepper_count[stepper_id] = 0;
   }
-  setStepperSpeed(stepper_id, magnitude, speed > 0);
 }
 void loop() {
 
