13c93b7291bc7a3ce953a58980f49c258d20927b
Thomas Dodds <thomasmd1928@gmail.com>
2023-03-13 14:43:57 -0500

    FIRST WORKING TEST
    Arduino connects to drone, sets drone in command mode, sets height limit, takes off, and lands.

--- telloduino/telloduino.ino
+++ telloduino/telloduino.ino
@@ -1,17 +1,17 @@
 // "Good enough" attempt to get an Uno WiFi Rev2 to talk to a Tello drone
 // by Thomas Dodds for ATU IEEE
 
-//#include <SPI.h>
-//#include <WiFiNINA.h>
-//#include <WiFiUdp.h>
-//#include "wifisecrets.h"
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include <WiFiUdp.h>
+#include "wifisecrets.h"
 #include "protocol.h"
 
-// WiFiUDP udp;
+int status = WL_IDLE_STATUS;
+WiFiUDP udp;
 
 void setup() {
   Serial.begin(9600);
-  /*
   if (WiFi.status() == WL_NO_MODULE) {
     Serial.println("Communication with WiFi module failed!");
     while (true);
@@ -25,27 +25,51 @@ void setup() {
     // wait 10 seconds for connection:
     delay(10000);
   }
-  //Serial.println("Connected");
-  */
-  //udp.begin(TELLO_PORT);
+  Serial.println("Connected");
 
+  udp.begin(TELLO_RECV_PORT);
+
+  // command mode
+  Serial.print("command:\n\t[string packet]\n");
+  udp.beginPacket(TELLO_ADDR, TELLO_SEND_PORT);
+  udp.write("command");
+  udp.endPacket();
+  delay(1000);
+
+  // set height 30 inches
+  Serial.print("set_alt_limit:\n\t");
   Packet height(SET_ALT_LIMIT_CMD);
   height.add_byte(0x1e);
   height.add_byte(0x00);
   height.print_buffer(Serial);
 
+  udp.beginPacket(TELLO_ADDR, TELLO_SEND_PORT);
+  udp.write(height.buffer, height.size());
+  udp.endPacket();
+
+  // takeoff
+  Serial.print("takeoff:\n\t");
   Packet takeoff(TAKEOFF_CMD);
   takeoff.fixup();
   takeoff.print_buffer(Serial);
 
+  udp.beginPacket(TELLO_ADDR, TELLO_SEND_PORT);
+  udp.write(takeoff.buffer, takeoff.size());
+  udp.endPacket();
+
+  // wait 10 seconds
+  delay(10000);
+
+  // land
+  Serial.print("land:\n\t");
   Packet land(LAND_CMD);
   land.add_byte(0x00);
   land.fixup();
   land.print_buffer(Serial);
-  
-  //udp.beginPacket(TELLO_ADDR, TELLO_PORT);
-  //udp.write(pkt.buffer, pkt.size());
-  //udp.endPacket();
+
+  udp.beginPacket(TELLO_ADDR, TELLO_SEND_PORT);
+  udp.write(land.buffer, land.size());
+  udp.endPacket();  
 }
 
 void loop() {
