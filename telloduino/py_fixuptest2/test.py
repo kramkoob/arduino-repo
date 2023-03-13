c4a98a02144034e080f6b7b90d88680a7632a0c1
Thomas Dodds <thomasmd1928@gmail.com>
2023-03-13 13:16:36 -0500

    test main three commands
    as of this commit, command buffers in both programs match. flight attempt is next step

--- telloduino/py_fixuptest2/test.py
+++ telloduino/py_fixuptest2/test.py
@@ -7,11 +7,19 @@ from utils import *
 import crc
 
 def main():
-        pkt = Packet(SET_ALT_LIMIT_CMD)
-        pkt.add_byte(0x1e)  # 30m
-        pkt.add_byte(0x00)
-        self.send_packet(pkt)
-	print(":".join("{:02x}".format(ord(chr(c))) for c in pkt.get_buffer()))
+	pkt = Packet(SET_ALT_LIMIT_CMD)
+	pkt.add_byte(0x1e)  # 30m
+	pkt.add_byte(0x00)
+	print(":".join("{:02x}".format(ord(chr(c))) for c in pkt.get_buffer()).upper())
+
+	pkt = Packet(TAKEOFF_CMD)
+	pkt.fixup()
+	print(":".join("{:02x}".format(ord(chr(c))) for c in pkt.get_buffer()).upper())
+
+	pkt = Packet(LAND_CMD)
+	pkt.add_byte(0x00)
+	pkt.fixup()
+	print(":".join("{:02x}".format(ord(chr(c))) for c in pkt.get_buffer()).upper())
 
 if __name__ == "__main__":
 	main()
--- telloduino/telloduino.ino
+++ telloduino/telloduino.ino
@@ -1,21 +1,51 @@
 // "Good enough" attempt to get an Uno WiFi Rev2 to talk to a Tello drone
 // by Thomas Dodds for ATU IEEE
 
-// #include <SPI.h>
-// #include <WiFiNINA.h>
+//#include <SPI.h>
+//#include <WiFiNINA.h>
+//#include <WiFiUdp.h>
+//#include "wifisecrets.h"
 #include "protocol.h"
-// #include "wifisecrets.h"
+
+// WiFiUDP udp;
 
 void setup() {
-  // open serial
   Serial.begin(9600);
+  /*
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    while (true);
+  }
+  
+  while (status != WL_CONNECTED) {
+    Serial.print("Connecting: ");
+    Serial.println(TELLO_WIFI_SSID);
+    status = WiFi.begin(TELLO_WIFI_SSID, TELLO_WIFI_PASS);
 
-  // create packet
-  Packet pkt(TAKEOFF_CMD);
-  pkt.fixup();
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  //Serial.println("Connected");
+  */
+  //udp.begin(TELLO_PORT);
 
-  // print packet buffer to serial
-  pkt.print_buffer(Serial);
+  Packet height(SET_ALT_LIMIT_CMD);
+  height.add_byte(0x1e);
+  height.add_byte(0x00);
+  height.print_buffer(Serial);
+
+  Packet takeoff(TAKEOFF_CMD);
+  takeoff.fixup();
+  takeoff.print_buffer(Serial);
+
+  Packet land(LAND_CMD);
+  land.add_byte(0x00);
+  land.fixup();
+  land.print_buffer(Serial);
+  
+  //udp.beginPacket(TELLO_ADDR, TELLO_PORT);
+  //udp.write(pkt.buffer, pkt.size());
+  //udp.endPacket();
 }
 
 void loop() {
